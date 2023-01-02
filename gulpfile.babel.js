c462a10b75c1d155aa19990704699df572d5ea65
Erik Roth <codeteam@rltechs.com>
2023-01-02 15:34:53 -0700

    add header and comment layout

---
+++ gulpfile.babel.js
@@ -0,0 +1,28 @@
+/** 
+ * File: gulpfile.babel.js
+ * 
+ * Author: Erik Roth for RL Technologies LLC <erikroth@rltechs.com>
+ * Link: https://codeteam.rltechsllc.com <codeteam@rltechs.com>
+ * Copyright 2020-2023 RL Technologies LLC. All Rights Reserved.
+ * 
+ */
+
+/* eslint-env es6 */
+/* eslint no-undef: "off" */
+'use strict';
+
+/** Retrieve Theme Settings */
+
+/** Retrieve Directory Info */
+
+/** External Dependencies */
+import * as g from 'gulp';
+
+/** Internal Dependencies */
+
+/** Build Gulp Tasks */
+
+/** Export Tasks */
+/** Default Export Task */
+
+/** End of File */
