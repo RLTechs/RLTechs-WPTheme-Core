5dc50cb1063324b7afb20b83d5fe4fa32c101a15
Erik Roth <codeteam@rltechs.com>
2023-01-03 01:49:37 -0700

    add header

---
+++ gulp/wpreq.js
@@ -0,0 +1,17 @@
+/**
+ * File: gulp/wpreq.js
+ *
+ * Author: Erik Roth for RL Technologies LLC <erikroth@rltechs.com>
+ * Link: https://rltechsllc.com <codeteam@rltechs.com>
+ * Copyright 2020-2023 RL Technologies LLC. All Rights Reserved.
+ *
+ */
+
+/* eslint-env es6 */
+/* eslint no-undef: "off" */
+'use strict';
+
+/** External Dependencies */
+import * as g from 'gulp';
+
+/** Internal Dependencies */
