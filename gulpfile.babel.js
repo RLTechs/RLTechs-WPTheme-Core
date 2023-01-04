9e73a3474dda82a471a664866e79968a2af176cf
Erik Roth <codeteam@rltechs.com>
2023-01-04 08:15:00 -0700

    add import-export for graphics functions

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,6 +19,7 @@
 import { parallel, series } from 'gulp';
 
 /** Internal Dependencies */
+import { graphics } from './gulp/graphics';
 import { fonts } from './gulp/fonts';
 import { php } from './gulp/php';
 import { vend, wipeVend } from './gulp/vendors';
@@ -34,6 +35,6 @@ import { wpreq } from './gulp/wpreq';
 //export default gDev;
 
 /** Export All Imports as Tasks */
-export { vend, wipeVend, wpreq, php, fonts };
+export { vend, wipeVend, wpreq, php, fonts, graphics };
 
 /** End of File */
