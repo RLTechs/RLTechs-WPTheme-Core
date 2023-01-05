3a943c9e61650d11daeef56f226b402ecded1751
Erik Roth <codeteam@rltechs.com>
2023-01-05 06:48:11 -0700

    add body tag so browsersync can be tested

---
+++ src/php/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * File: index.php
+ *
+ * @author Erik Roth for RL Technologies LLC <erikroth@rltechs.com>
+ * @link https://rltechsllc.com
+ * @copyright 2020-2023 RL Technologies LLC. All Rights Reserved.
+ * @license
+ *
+ * @package RLTechs/ThemeCore
+ * @version 1.0.0
+ */
+
+/**
+ * Get header file
+ * @since 1.0.0
+ */
+//get_header(); // TODO add multiple types of Header files for selection in options
+?>
+
+<body>
+
+TEST<br/>
+TEST<br/>
+TEST<br/>
+TEST it yo!
+</body>
