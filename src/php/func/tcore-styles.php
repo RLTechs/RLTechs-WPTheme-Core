a79479882c8e6e25548516f6d4e5354880d154d4
Erik Roth <codeteam@rltechs.com>
2023-01-09 09:59:01 -0700

    add css files

--- src/php/func/tcore-styles.php
+++ src/php/func/tcore-styles.php
@@ -7,7 +7,7 @@
  * copyright 2020-2023 RL Technologies LLC. All Rights Reserved.
  * license
  *
- * package RLTechs/MyShul
+ * package RLTechs/tcore
  * version 1.0.0
  */
 
@@ -16,7 +16,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 };
 
 function tcore_styles() {
-    //
+    /** Register Stylesheets */
+    $tcore_main_stylesheet = TCORE_AURI . 'themecore.css';
+	wp_register_style( 'tcore-main', $tcore_main_stylesheet, array(), null);
+
+    $tcore_bsi_stylesheet = TCORE_AURI . 'bs-icons.css';
+	wp_register_style( 'tcore-bsi', $tcore_bsi_stylesheet, array(), null);
+
+    $tcore_fa_stylesheet = TCORE_AURI . 'fontawesome.css';
+	wp_register_style( 'tcore-fa', $tcore_fa_stylesheet, array(), null);
+
+    /** Enqueue Files IN ORDER */
+    wp_enqueue_style('tcore-main');
+    wp_enqueue_style('tcore-bsi');
+    wp_enqueue_style('tcore-fa');
 
 }
 
