87b93bd7fe9ecbafe39fcf06dc3fd160a1435daa
Erik Roth <codeteam@rltechs.com>
2023-01-09 09:39:12 -0700

    add die index

--- src/php/index.php
+++ src/php/index.php
@@ -27,13 +27,48 @@ namespace RLTechs\ThemeCore;
     <meta charset="<?php bloginfo( 'charset' ); ?>" />  
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-
+    <!-- FavIcons -->
 
     
+    <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+
+  <?php wp_body_open(); ?>
+
+  
+  <div id="page" class="site">
+
+    <header id="masthead" class="site-header">
+
+
+
+    </header>
+
+    <div id="content" class="site-content container py-5 mt-4">
+    
+      <div id="primary" class="content-area">
+
 A
+
+
+      </div>
+
+
+
+    </div>
+
+    <?php //get_footer(); ?>
+
+    <footer>
+
+
+
+    </footer>
+  </div>
+
+  <?php wp_footer(); ?>
 
 </body>
 
