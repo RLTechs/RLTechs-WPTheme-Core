46483d07b5b3295953e848498b5ba4d72e49fc0d
Erik Roth <codeteam@rltechs.com>
2023-01-10 00:29:33 -0700

    add body classes

--- src/php/func/tcore-xtra-funcs.php
+++ src/php/func/tcore-xtra-funcs.php
@@ -14,3 +14,24 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 };
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function tcore_body_class($classes) {
+    // Adds a class of hfeed to non-singular pages.
+    if (!is_singular()) {
+      $classes[] = 'hfeed';
+    }
+  
+    // Adds a class of no-sidebar when there is no sidebar present.
+    if (!is_active_sidebar('sidebar-1')) {
+      $classes[] = 'no-sidebar';
+    }
+  
+    return $classes;
+  }
+  add_filter('body_class', 'tcore_body_class');
